a3d68defd8f71d60cbb510293e1fcd40862bf6b6
Victor San Kho Lin <victor@sankholin.com>
2023-10-02 20:51:12 +1100

    Added initial CDK Pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,6 +1,7 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Repository} from "aws-cdk-lib/aws-codecommit";
 import {Construct} from "constructs";
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class WorkshopPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,6 +12,15 @@ export class WorkshopPipelineStack extends Stack {
             repositoryName: "WorkshopRepo",
         });
 
-        // Pipeline code goes here
+        // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+        const pipeline = new CodePipeline(this, "Pipeline", {
+            pipelineName: "WorkshopPipeline",
+            synth: new CodeBuildStep("SynthStep", {
+                input: CodePipelineSource.codeCommit(repo, "main"),
+                installCommands: ["npm i -g npm@latest"],
+                commands: ["npm ci", "npm run build", "npx cdk synth"],
+            }),
+        });
     }
 }
