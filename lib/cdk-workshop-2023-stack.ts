30a74a754362cdb39e622ba4ed5afc5ceafd1520
Victor San Kho Lin <victor@sankholin.com>
2023-10-02 22:05:01 +1100

    Polished  pipeline: CfnOutput endpoints

--- lib/cdk-workshop-2023-stack.ts
+++ lib/cdk-workshop-2023-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import {CfnOutput, Stack, StackProps} from "aws-cdk-lib";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
@@ -6,6 +6,10 @@ import { HitCounter } from "./hitcounter";
 import {TableViewer} from "cdk-dynamo-table-viewer";
 
 export class CdkWorkshop2023Stack extends Stack {
+  // ADD THIS
+  public readonly hcViewerUrl: CfnOutput;
+  public readonly hcEndpoint: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -28,6 +32,15 @@ export class CdkWorkshop2023Stack extends Stack {
     const tv = new TableViewer(this, 'ViewHitCounter', {
       title: 'Hello Hits',
       table: helloWithCounter.table
+    });
+
+    // ADD THIS
+    this.hcEndpoint = new CfnOutput(this, "GatewayUrl", {
+      value: gateway.url,
+    });
+
+    this.hcViewerUrl = new CfnOutput(this, "TableViewerUrl", {
+      value: tv.endpoint,
     });
   }
 }
