a65f99a08ff5801b53b2eb5c65f1da6d510397f6
Victor San Kho Lin <victor@sankholin.com>
2023-10-02 22:11:03 +1100

    Polished  pipeline: Added validation test

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,12 +1,17 @@
-import {Stage, StageProps} from 'aws-cdk-lib';
+import {CfnOutput, Stage, StageProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {CdkWorkshop2023Stack} from "./cdk-workshop-2023-stack";
 
 export class WorkshopPipelineStage extends Stage {
+    public readonly hcViewerUrl: CfnOutput;
+    public readonly hcEndpoint: CfnOutput;
+
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
         const service = new CdkWorkshop2023Stack(this, "WebService");
-    }
 
+        this.hcEndpoint = service.hcEndpoint;
+        this.hcViewerUrl = service.hcViewerUrl;
+    }
 }
